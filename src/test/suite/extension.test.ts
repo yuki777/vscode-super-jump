fc32da8e4362e194ed7527e11673ea259ed1c942
Yuki Adachi <yuki777@gmail.com>
2024-05-24 22:04:46 +0700

    テストを追加する

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -10,6 +10,8 @@ suite('Extension Test Suite', () => {
       {
         "targetLanguages": [
           "php",
+          "twig",
+          "shellscript"
         ],
         "regex": "(get|post|put|delete|resource|uri|ResourceParam|Embed)\\(.*?app:\\/\\/self\\/([^'\"\\{\\?#]*)",
         "searchFileName": "$2",
@@ -17,7 +19,26 @@ suite('Extension Test Suite', () => {
           "pascalCase"
         ],
         "searchDirectories": [
-          "src/test/dummy/App"
+          "src/Resource/App"
+        ],
+        "searchFileExtension": ".php"
+      },
+      {
+        "targetLanguages": [
+          "php",
+          "twig",
+          "shellscript"
+        ],
+        "regex": "(get|post|put|delete|resource|uri|ResourceParam|Embed)\\(.*?page:\\/\\/self\\/([^'\"\\{\\?#]*)",
+        "searchFileName": "$2",
+        "searchFileNameConvertRules": [
+          "pascalCase"
+        ],
+        "searchDirectories": [
+          "src/Resource/Page",
+          "src/Resource/Page/Admin",
+          "src/Resource/Page/Cli",
+          "src/Resource/Page/Content"
         ],
         "searchFileExtension": ".php"
       }
@@ -25,12 +46,28 @@ suite('Extension Test Suite', () => {
     const provider = new PeekFileDefinitionProvider(configs);
     assert(provider instanceof PeekFileDefinitionProvider, 'provider should be an instance of PeekFileDefinitionProvider');
 
-    const document = {
-      getText: (range: vscode.Range) => "$this->resource->get('app://self/Article/Valid?foo=FOO');",
-      getWordRangeAtPosition: (position: vscode.Position, regex: RegExp) => new vscode.Range(position, position.translate(0, 25))
+    let testText = "$this->resource->get('app://self/Article/Valid?foo=FOO');";
+    let cursorPosition = 25;
+    let expectTargetFile = ["src/Resource/App/Article/Valid.php"];
+    let expectCount = 1;
+    let document = {
+      getText: (range: vscode.Range) => testText,
+      getWordRangeAtPosition: (position: vscode.Position, regex: RegExp) => new vscode.Range(position, position.translate(0, cursorPosition))
     } as vscode.TextDocument;
-    const targetFiles = provider.getTargetFiles(document, new vscode.Position(0, 0));
-    assert.strictEqual(targetFiles.length, 1);
-    assert.deepStrictEqual(targetFiles, ['src/test/dummy/App/Article/Valid.php']);
+    let targetFiles = provider.getTargetFiles(document, new vscode.Position(0, 0));
+    assert.strictEqual(targetFiles.length, expectCount);
+    assert.deepStrictEqual(targetFiles, expectTargetFile);
+
+    testText = "$this->resource->get('page://self/Article/Valid?foo=FOO');";
+    cursorPosition = 25;
+    expectTargetFile = ["src/Resource/Page/Article/Valid.php", "src/Resource/Page/Admin/Article/Valid.php", "src/Resource/Page/Cli/Article/Valid.php", "src/Resource/Page/Content/Article/Valid.php"];
+    expectCount = 4;
+    document = {
+      getText: (range: vscode.Range) => testText,
+      getWordRangeAtPosition: (position: vscode.Position, regex: RegExp) => new vscode.Range(position, position.translate(0, cursorPosition))
+    } as vscode.TextDocument;
+    targetFiles = provider.getTargetFiles(document, new vscode.Position(0, 0));
+    assert.strictEqual(targetFiles.length, expectCount);
+    assert.deepStrictEqual(targetFiles, expectTargetFile);
   });
 });
