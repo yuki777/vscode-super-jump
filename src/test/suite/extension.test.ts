e6994b0f3c11b2b22f4421f4fd3c97aa20b5f4f8
Yuki Adachi <yuki777@gmail.com>
2024-05-27 22:37:13 +0700

    グループごとにテスト実行可能にする

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,9 +1,18 @@
 import * as assert from 'assert';
 import * as vscode from 'vscode';
 import PeekFileDefinitionProvider from '../../PeekFileDefinitionProvider';
+import { suite, test } from 'mocha';
 
 suite('Extension Test Suite', () => {
   vscode.window.showInformationMessage('Start all tests.');
+
+  test('sample fail test', () => {
+    assert.strictEqual([], 0);
+  });
+
+  test('sample success test', () => {
+    assert.deepStrictEqual([], []);
+  });
 
   test('getTargetFiles basics', () => {
     const configs = [
@@ -58,17 +67,25 @@ suite('Extension Test Suite', () => {
     const provider = new PeekFileDefinitionProvider(configs);
     assert(provider instanceof PeekFileDefinitionProvider, 'provider should be an instance of PeekFileDefinitionProvider');
 
+    // Init
+    let testText = "";
+    let cursorPosition = 0;
+    let expectTargetFile: string[] = [];
+    let expectCount = 0;
+    let document = {} as vscode.TextDocument;
+    let targetFiles = [];
+
     // Test 'app'
-    let testText = "$this->resource->get('app://self/Article/Valid?foo=FOO');";
-    let cursorPosition = 25;
-    let expectTargetFile = ["src/Resource/App/Article/Valid.php"];
-    let expectCount = 1;
-    let document = {
+    testText = "$this->resource->get('app://self/Article/Valid?foo=FOO');";
+    cursorPosition = 25;
+    expectTargetFile = ["src/Resource/App/Article/Valid.php"];
+    expectCount = 1;
+    document = {
       getText: (range: vscode.Range) => testText,
       getWordRangeAtPosition: (position: vscode.Position, regex: RegExp) => new vscode.Range(position, position.translate(0, cursorPosition))
     } as vscode.TextDocument;
 
-    let targetFiles = provider.getTargetFiles(document, new vscode.Position(0, 0));
+    targetFiles = provider.getTargetFiles(document, new vscode.Position(0, 0));
     assert.strictEqual(targetFiles.length, expectCount);
     assert.deepStrictEqual(targetFiles, expectTargetFile);
 
@@ -93,6 +110,46 @@ suite('Extension Test Suite', () => {
       "var/json_validate/bar.get.json",
     ];
     expectCount = 2;
+    document = {
+      getText: (range: vscode.Range) => testText,
+      getWordRangeAtPosition: (position: vscode.Position, regex: RegExp) => new vscode.Range(position, position.translate(0, cursorPosition))
+    } as vscode.TextDocument;
+    targetFiles = provider.getTargetFiles(document, new vscode.Position(0, 0));
+    assert.strictEqual(targetFiles.length, expectCount);
+    assert.deepStrictEqual(targetFiles, expectTargetFile);
+  });
+
+  test('json schema test', () => {
+    const configs = [
+      {
+        "targetLanguages": ["php"],
+        "regex": "#\\[JsonSchema\\(.*?schema\\s?:\\s?'([^']*)'.*?\\)\\]",
+        "searchFileName": "$1",
+        "searchDirectories": ["var/json_schema"]
+      },
+      {
+        "targetLanguages": ["php"],
+        "regex": "#\\[JsonSchema\\(.*?params\\s?:\\s?'([^']*)'.*?\\)\\]",
+        "searchFileName": "$1",
+        "searchDirectories": ["var/json_validate"]
+      },
+    ];
+    const provider = new PeekFileDefinitionProvider(configs);
+    assert(provider instanceof PeekFileDefinitionProvider, 'provider should be an instance of PeekFileDefinitionProvider');
+
+    // Init
+    let testText = "";
+    let cursorPosition = 0;
+    let expectTargetFile: string[] = [];
+    let expectCount = 0;
+    let document = {} as vscode.TextDocument;
+    let targetFiles = [];
+
+    // Test JsonSchema
+    testText = "#[JsonSchema(schema: 'foo.get.json', key: 'announcement', params: 'bar.get.json')]";
+    cursorPosition = 1;
+    expectTargetFile = [];
+    expectCount = 0;
     document = {
       getText: (range: vscode.Range) => testText,
       getWordRangeAtPosition: (position: vscode.Position, regex: RegExp) => new vscode.Range(position, position.translate(0, cursorPosition))
